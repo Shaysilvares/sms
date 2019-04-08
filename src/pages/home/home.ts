5d609fa5b8aa90fa32a64d9b6c8882b97af82f32
Shayna bauer <shayna.bauer@acolconsultoria.com>
2019-04-08 16:15:25 -0300

    mandando sms pra uma lista de numeros

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,36 +1,67 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
 import { SMS } from '@ionic-native/sms';
 import { AndroidPermissions } from '@ionic-native/android-permissions';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  public telefone: Array<string> = ["21982828022", "21969982508"];
+  //public telefone: Array<string> = [];
+  public telefone: string;
   public mensagem: string;
+  public formulario: FormGroup;
+  public toast: any;
 
-  constructor(public navCtrl: NavController, public sms: SMS, public androidPermissions: AndroidPermissions) {
+  constructor(
+    public navCtrl: NavController, 
+    public sms: SMS, 
+    public androidPermissions: AndroidPermissions,
+    public formBuilder: FormBuilder,
+    public toastCtrl: ToastController) {
+
+      this.formulario = this.formBuilder.group({
+        telefone: ['', Validators.required],
+        mensagem: ['', Validators.required]
+      })
 
   }
 
 
-  enviarSMS() {
+  enviarSMS() {    
     let options: {
       replaceLineBreaks: true,
       android: {
         intent: 'INTENT'
       }
     }
-
+    
     this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.SEND_SMS).then(() => {
-      console.log(typeof this.telefone);
-      this.sms.send(this.telefone, this.mensagem, options).then(() => {
-        console.log("enviou");
-        }).catch((error) => {
-          console.log(JSON.stringify(error));
+      
+      let telArray = this.telefone.split(",");
+
+      telArray.forEach(tel => {
+        this.sms.send(tel, this.mensagem, options).then(() => {
+          console.log("enviou");          
+          this.toast = this.toastCtrl.create({
+            message: 'Seu SMS foi enviado com sucesso!',
+            duration: 6000,
+            position: "bottom"
+          });
+          this.toast.present();
+          }).catch((error) => {
+            console.log(JSON.stringify(error));
+            this.toast = this.toastCtrl.create({
+              message: 'Erro ao enviar o SMS',
+              duration: 6000,
+              position: "bottom"
+            });
+            this.toast.present();
+        });
       });
+
     }).catch((err)=> {
       console.log(err);
     });
